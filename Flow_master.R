81625b43feb37b10a5cebe200d0a82cf286dda87
Lindsay Mack <38704476+lmack5@users.noreply.github.com>
2018-04-25 13:29:32 -0500

    summary

---
+++ Flow_master.R
@@ -0,0 +1,21 @@
+library(readxl)
+library(dplyr)
+library(plyr)
+
+###################################################################################
+#Flow data
+#Create data frame for daily data
+#file.list <- list.files(pattern = '*.xlsx')
+#df.list <- lapply(file.list, read_excel)
+#daily.df <- ldply(df.list, data.frame)
+
+#Save as .csv
+#write.csv(daily.df, "daily flow.csv")
+
+#Create data frame for monthly data
+#file.list2 <- list.files(pattern = '*.xlsx')
+#df.list2 <- lapply(file.list2, read_excel)
+#monthly.df <- ldply(df.list2, data.frame)
+
+#Save as .csv
+#write.csv(monthly.df, "monthly flow.csv")
